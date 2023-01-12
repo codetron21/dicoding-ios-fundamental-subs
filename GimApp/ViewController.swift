64a69dd94dfaecc39f1865b43f3a7b08e8df4436
codetron <codetronclan@gmail.com>
2023-01-12 19:01:18 +0700

    Initial Commit

---
+++ GimApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GimApp
+//
+//  Created by Apple on 12/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
